9d6eedd9bd84cdb2770cf6621ae37500e3f7fb2b
nantdev1 <110542767+nantdev1@users.noreply.github.com>
2022-11-01 16:26:02 +0000

    Update server.js

--- api/server.js
+++ api/server.js
@@ -2,7 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const data = require("./data.json")
+const cors = require("cors");
 
+// enable CORS
+app.use(cors());
 
 app.get("/", async (req, res) => {
   res.send(data)
